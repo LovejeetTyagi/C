8eceb7b6b6cf2e1ab6a1935d3d23793cb27db998
Lovejeet Tyagi <lovejeettyagi17@gmail.com>
2024-09-16 21:18:31 +0530

    Add files via upload

---
+++ ques 1.105.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+int main()
+{
+ printf("%20s", "/");
+ printf("%s\n", "\\");
+
+ printf("%19s", "/");
+ printf("%3s\n", "\\");
+ printf("%18s", "/");
+ printf("%5s\n", "\\");
+ printf("%17s", "/");
+ printf("%7s\n", "\\");
+ printf("%16s", "/");
+ printf("%9s\n", "\\");
+ printf("%15s", "/");
+ printf("%11s\n", "\\");
+ printf("%14s", "/");
+ printf("%13s\n", "\\");
+ printf("%13s", "/");
+ printf("%15s\n", "\\");
+ printf("%12s", "/");
+ printf("%17s\n", "\\");
+ printf("%11s", "/");
+ printf("%19s\n", "\\");
+ printf("%10s", "/");
+ 
+ 
+//  printf("%s", "_ ");
+ printf("%s", "_ ");
+ printf("%s", "_ ");
+ printf("%s", "_ ");
+ printf("%s", "_ ");
+ printf("%s", "_ ");
+ printf("%s", "_ ");
+ printf("%s", "_ ");
+ printf("%s", "_ ");
+ printf("%s", "_ ");
+ printf("%s", "_ ");
+ printf("%-21s", "\\");
+
+ 
+ 
+return 0;
+
+}
